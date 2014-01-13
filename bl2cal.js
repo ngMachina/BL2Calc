9e82987c881c948569e5fe7def93c0b453c83115
Michael O'Connor <mjoconnor83@gmail.com>
2014-01-12 20:26:58 -0700

    Add fire type, multiple rounds and second weapon

--- bl2cal.js
+++ bl2cal.js
@@ -1,47 +1,53 @@
 function BL2Ctrl($scope) {
-	$scope.semiCap = function() {
-		rate = $scope.fireRate;
+	$scope.dmgrounds = 1;
+	$scope.dmgrounds2 = 1;
+	$scope.fireType = ["Semi-Auto","Automatic","Burst"];
 
-		if(rate > 5.4) {
-			rate = 5.4;
+	$scope.semiCap = function(fireRate) {
+		var rate = fireRate;
+
+		if($scope.fireType == "Semi-Auto") {
+			if(rate > 5.4) {
+				rate = 5.4;
+			}
 		}
 
 		return rate;
 	};
 
-	$scope.adjAcc = function() {
-		return $scope.accuracy/100;
+	$scope.adjAcc = function(acc) {
+		return acc/100;
 	};
 
-	$scope.dps = function() {
-		return $scope.damage * $scope.semiCap();
+	$scope.dps = function(dmg,rounds,fireRate) {
+		return (dmg*rounds)*$scope.semiCap(fireRate);
 	};
 
-	$scope.dpsacc = function() {
-		return (Math.floor(($scope.semiCap() * 60)*$scope.adjAcc())*$scope.damage)/60;
+	$scope.dpsacc = function(dmg,rounds,acc,fireRate) {
+		return (Math.floor((($scope.semiCap(fireRate)*rounds)*60)*$scope.adjAcc(acc))*dmg)/60;
 	};
 
-	$scope.spm = function() {
-		var fireCycle = $scope.magSize/$scope.semiCap();
-		var cyclesMinute = 60/(fireCycle + parseFloat($scope.reloadSpeed));
+	$scope.spm = function(magSize,fireRate,reload) {
+		var fireCycle = magSize/$scope.semiCap(fireRate);
+		var cyclesMinute = 60/(fireCycle+parseFloat(reload));
 		var fullCycles = Math.floor(cyclesMinute);
-		var remaining = (cyclesMinute - fullCycles)*(fireCycle + parseFloat($scope.reloadSpeed));
+		var remaining = (cyclesMinute-fullCycles)*(fireCycle+parseFloat(reload));
 		var partCycle = 0;
 
 		if(fireCycle < remaining) {
 			fullCycles++;
 		} else {
-			partCycle = Math.floor((remaining/fireCycle)*$scope.magSize);
+			partCycle = Math.floor((remaining/fireCycle)*magSize);
 		}
 
-		return ($scope.magSize * fullCycles) + parseFloat(partCycle);
+		return (magSize*fullCycles)+parseFloat(partCycle);
 	};
 
-	$scope.susDps = function() {
-		return ($scope.spm()*$scope.damage)/60;
+	$scope.susDps = function(dmg,rounds,magSize,fireRate,reload) {
+		return ($scope.spm(magSize,fireRate,reload)*(dmg*rounds))/60;
 	};
 
-	$scope.susAcc = function() {
-		return (Math.floor($scope.spm()*$scope.adjAcc())*$scope.damage)/60;
+	$scope.susAcc = function(dmg,rounds,acc,magSize,fireRate,reload) {
+		return (Math.floor(($scope.spm(magSize,fireRate,reload)*rounds)*$scope.adjAcc(acc))*dmg)/60;
 	};
 }
